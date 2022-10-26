8d5f7f8cbe6e6f256fcbc10e6972c1d890f221e1
hoanggminh2702 <hoanggminh2702@gmail.com>
2022-10-26 15:57:47 +0700

    get content html server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const https = require("https");
+const port = 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/get-content", (req, res) => {
+  const url = req.body.url ?? "";
+      let data = "";
+
+  if (url) {
+
+    https.get(url, (resp) => {
+
+      // A chunk of data has been received.
+      resp.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on("end", () => {
+        data.toString();
+        res.status(200).send(data);
+      });
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log("Listening on port " + port);
+});
