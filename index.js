60138abe7cbc488b4aa66402b344722547e136f2
hoanggminh2702 <hoanggminh2702@gmail.com>
2022-10-26 16:01:03 +0700

    add enable cors

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const https = require("https");
 const port = 8080;
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 app.post("/get-content", (req, res) => {
