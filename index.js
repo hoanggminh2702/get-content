2d6b3bb8ba85ad94ed1f520f6286be05b3e386a3
hoanggminh2702 <hoanggminh2702@gmail.com>
2022-10-26 16:18:49 +0700

    add handle with both http and https

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
 const express = require("express");
 const cors = require("cors");
 const https = require("https");
+const http = require("http");
 const port = 8080;
 
 const app = express();
-app.use(cors({
-  origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.post("/get-content", (req, res) => {
   const url = req.body.url ?? "";
-      let data = "";
+  let data = "";
 
   if (url) {
+    try {
+      http.get(url, (resp) => {
+        // A chunk of data has been received.
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
 
-    https.get(url, (resp) => {
-
-      // A chunk of data has been received.
-      resp.on("data", (chunk) => {
-        data += chunk;
+        // The whole response has been received. Print out the result.
+        resp.on("end", () => {
+          data.toString();
+          return res.status(200).send(data);
+        });
       });
+    } catch (err) {
+      https
+        .get(url, (response) => {
+          // A chunk of data has been received.
+          response.on("data", (chunk) => {
+            data += chunk;
+          });
 
-      // The whole response has been received. Print out the result.
-      resp.on("end", () => {
-        data.toString();
-        res.status(200).send(data);
-      });
-    });
+          // The whole response has been received. Print out the result.
+          response.on("end", () => {
+            data.toString();
+            return res.status(200).send(data);
+          });
+        })
+        .on("error", (err) => {
+          return res.status(500).send(err);
+        });
+    }
   }
 });
 
